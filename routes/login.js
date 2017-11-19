8ba98fe11132cfb7c037fcab7d951bc62e9d121f
steven <steveh@mail.sfsu.edu>
2017-11-19 10:28:49 -0800

    added POST on login and registration

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET users listing. */
 router.get('/', (request, respond) => {
   respond.render('login', { title: 'Login' });
+});
+
+router.post('/', (request, respond) => {
+  console.log('POST request on /login');
+  const confirm = '';
+
+  db.any(confirm);
 });
 
 module.exports = router;
--- routes/registration.js
+++ routes/registration.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET users listing. */
 router.get('/', (request, respond) => {
   respond.render('registration', { title: 'Registration Page' });
+});
+
+router.post('/', (request, respond) => {
+  console.log('POST request on /registration');
+
+  if (request.body.password !== request.body.confirmPassword) {
+    console.log('redirecting...')
+    respond.redirect('/registration');
+  } else {
+    console.log('Username: ' + request.body.username);
+    console.log('Password: ' + request.body.password);
+    const insert = 'INSERT INTO players (username, password) VALUES \
+    (' + request.body.username + ', ' + request.body.password + ')';
+
+    db.any(insert);
+  }
 });
 
 module.exports = router;
