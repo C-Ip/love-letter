ab33170910f8461d37e8856e6a79baeb329c7acb
jerrya <jauyeung91@gmail.com>
2017-11-21 22:42:56 -0800

    wip

--- routes/registration.js
+++ routes/registration.js
@@ -11,19 +11,19 @@ router.post('/', (request, respond) => {
   console.log('POST request on /registration');
 
   // I'll just have page reload when passwords do not match for now
-  if (request.body.password !== request.body.confirmPassword) {
+  if (request.body.passwordReg !== request.body.confirmPassword) {
     console.log('redirecting...')
     respond.redirect('/registration');
   } else {
-    console.log('Username: ' + request.body.username);
-    console.log('Password: ' + request.body.password);
+    console.log('Username: ' + request.body.usernameReg);
+    console.log('Password: ' + request.body.passwordReg);
 /*
     const insert = 'INSERT INTO players(username, password) VALUES \
     (\"' + request.body.username + '\", \"' + request.body.password + '\");';
 */
 
 
-    db.none('INSERT INTO players(username,password) VALUES($1,$2)',[request.body.username, request.body.password])
+    db.none('INSERT INTO players(username,password) VALUES($1,$2)',[request.body.usernameReg, request.body.passwordReg])
     .then( _ => {
       console.log('successful');
     }).catch(error => {
