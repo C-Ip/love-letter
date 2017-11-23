c66485177f5e4dd7ce4ed6205d8ce5c451e684e4
Calvin Ip <ip@mail.sfsu.edu>
2017-11-22 23:02:38 -0800

    Game lobby can be created with values inserted into tabels, had to disabled chatbox for lobby.

--- routes/gamelobby.js
+++ routes/gamelobby.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET users listing. */
 router.get('/', (req, res) => {
   res.render('gamelobby', {title: 'Game Lobby Page'});
+});
+
+router.post('/', (request, response, next) => {
+  db.one('INSERT INTO game(playerturn) VALUES($1) RETURNING gameid', ['0'])
+  .then( data => {
+    console.log("Room created");
+    console.log(data.gameid);
+    var gameRoomId = data.gameid;
+    db.none('INSERT INTO playergame(playerid, gameid) VALUES($1, $2)', [request.session.player_id, gameRoomId])
+    .then( data => {
+      console.log("Player added to game lobby");
+    })
+  })
 });
 
 module.exports = router;
